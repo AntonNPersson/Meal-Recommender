87ca48f031de93a1a05be88968e26350777c643f
AntonNPersson <129338652+AntonNPersson@users.noreply.github.com>
2025-06-05 17:10:11 +0200

    Removed Secret key

--- Meal-recommender/telegram_app.py
+++ Meal-recommender/telegram_app.py
@@ -1,7 +1,9 @@
 from Backend.Services.telegram_service import TelegramBotService
 
 def main():
-    secret_api_key = "7757803111:AAHRr2uxq_RQHqt9Qn31VOHQdh2IKjU9kUA" # ITS SECRET, DO NOT SHARE! :D
+    secret_api_key = "" # Enter your Telegram Bot API key here
+    if not secret_api_key:
+        raise ValueError("Please set your Telegram Bot API key in the secret_api_key variable.")
     telegram_bot_service = TelegramBotService(secret_api_key)
 
     telegram_bot_service.start()
